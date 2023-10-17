acccd144cf92e344e93c16f642cbc4a4e04062a8
MarleneSchiappaa <max.tonnadre@efrei.net>
2023-10-17 10:53:54 -0600

    ca mange pas de pain

--- src/main/java/com/example/ticketmachine/HelloApplication.java
+++ src/main/java/com/example/ticketmachine/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("TicketChoose.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Ticket Machine!");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/example/ticketmachine/TicketChooseController.java
@@ -0,0 +1,5 @@
+package com.example.ticketmachine;
+
+public class TicketChooseController {
+
+}
